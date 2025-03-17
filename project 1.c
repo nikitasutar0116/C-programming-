5e2d4f28a7482d4c772931e13a7dd22c16734910
nikitasutar0116 <nikitasutar73176@gmail.com>
2025-03-17 15:59:59 +0530

    Add files via upload

---
+++ project 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  printf("Redy To Play...");
+  printf("\nOpen a calculator and take pen and paper...");
+  printf("\nLet's Start !");
+  printf("\nMultiply with 5, Your 1st digit of age in your mind.");
+  printf("\nex :- If your age is 22 the 1st digit of your age is 2 * 5 =10");
+  printf("\nNow add 4 with the Result 1 in your mind !");
+  printf("\nNow double the Result 2 !");
+  printf("\nNow add 2nd digit of your age !");
+  int n;
+  printf("Enter Your Final Result : ");
+  scanf("\n%d",&n);
+  n-=8;
+  printf("\n%d Is Your Age !",n);
+  printf("\nEnjoy and play again...");
+  printf("\nBye Bye!");
+  return 0;
+}
